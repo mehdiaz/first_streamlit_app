660db5190969f003b9021192ffbee84add70bc5b
mehdiaz <sidiboumedine@gmail.com>
2022-10-11 09:48:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ streamlit.header('🥑 Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('Build your own fruit menu")
+
+streamlit.dataframe(my_fruit_list)
